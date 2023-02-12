17f5cfa0663083d6612b67ab2d73a03b51cded03
Drewrey Lupton <drewrey@users.noreply.github.com>
2023-02-12 17:34:28 -0500

    start to implement search via the form and DB
    Need to determine interplay between:
    - browser history on search
    - populating data based on search params on initial load
    - updating results on subsequent searches

--- src/routes/recipes/search/+page.server.js
+++ src/routes/recipes/search/+page.server.js
@@ -1,41 +1,24 @@
 import { error } from '@sveltejs/kit';
 
-// import { TYPESENSE_API_HOST, TYPESENSE_API_KEY } from '$env/static/private';
-
-import data from '$lib/data/search_banana_pudding.json';
+import db from '$lib/data/db.js';
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load(event) {
-	console.log('event', event);
-	// console.log('URL', event.request.url.searchParams);
-	// console.log('BANANA', $page.url.searchParams);
+export async function load({ url }) {
+	let responseData = [];
 
-	let responseData;
-
-	if (!event.request.url.search) {
-		responseData = { results: [] };
-	} else {
-		responseData = data;
-		// responseData = await search({ query: event.request.url.search.toString() });
+	if (url.searchParams.has('q')) {
+		responseData = await searchRecipes({ query: url.searchParams.get('q') });
 	}
 
-	if (!responseData) {
-		throw error(500, {
-			message: 'Error searching'
-		});
-	}
-
-	console.log(data);
-
-	return { data: responseData };
+	return { results: responseData };
 }
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-	default: async (event) => {
-		console.log('event', event);
+	default: async ({ request }) => {
+		const formData = await request.formData();
 
-		// const data = await search({ query: event.request.url.search.toString() });
+		const data = await searchRecipes({ query: formData.get('q') });
 
 		if (!data) {
 			throw error(500, {
@@ -43,43 +26,25 @@ export const actions = {
 			});
 		}
 
-		return { data };
+		return { results: data };
 	}
 };
 
 /**
- * @typedef {{
- *      query: string
- * }} SearchParam
+ * @param {*} param0
  */
-
-/**
- *
- * @param params {SearchParam}
- */
-// async function search({ query }) {
-// 	await fetch(
-// 		`https://${TYPESENSE_API_HOST}/multi_search?x-typesense-api-key=${TYPESENSE_API_KEY}`,
-
-// 		{
-// 			method: 'POST',
-// 			headers: { 'Content-Type': 'application/json' },
-// 			body: JSON.stringify({
-// 				searches: [
-// 					{
-// 						query_by: 'title',
-// 						sort_by: '',
-// 						highlight_full_fields: 'title',
-// 						collection: 'r',
-// 						q: `${query}`,
-// 						facet_by: 'ingredient_names',
-// 						filter_by: '',
-// 						max_facet_values: 100,
-// 						page: 1,
-// 						per_page: 15
-// 					}
-// 				]
-// 			})
-// 		}
-// 	).then((r) => r.json());
-// }
+async function searchRecipes({ query }) {
+	return new Promise((resolve, reject) => {
+		// TODO: add pagination
+		db.execute(
+			"SELECT * FROM recipes WHERE title LIKE concat('%', ?, '%') limit 10",
+			[query],
+			(err, res) => {
+				if (err) {
+					reject(err);
+				}
+				resolve(res);
+			}
+		);
+	});
+}
--- src/routes/recipes/search/+page.svelte
+++ src/routes/recipes/search/+page.svelte
@@ -1,34 +1,53 @@
 <script>
 	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 
 	/** @type {import('./$types').ActionData} */
 	export let form;
+
+	// TODO: this is ugly, need to decide how to separate form/data
+	const syncFormToData = (some) => {
+		form = some;
+	};
 </script>
 
 <h1>Search</h1>
 <form method="POST" use:enhance>
-	<input type="search" name="search" /><button type="submit">Search</button>
+	<input type="search" name="q" /><button type="submit">Search</button>
 </form>
 
-{#if form?.data}
-	{#if Object.keys(data.data).includes('results')}
-		{#if data.data.results[0].hits.length === 0}
-			<p>no results</p>
-		{:else}
-			<div class="grid">
-				{#each data.data.results[0].hits as hit}
-					<div>
-						<h2>{hit.document.title}</h2>
-						<p>{hit.document.directions.join(', ')}</p>
-					</div>
-					<a href="/recipes/{hit.document.id}">View Recipe</a>
-				{/each}
+{#await data}
+	<p>loading...</p>
+{:then data}
+	{syncFormToData(data)}
+	{#if form && form.results}
+		{#each form.results as result}
+			<div>
+				<h2>{result.title}</h2>
+				<p>{result.directions}</p>
 			</div>
-		{/if}
-	{:else}
-		<p>no search results</p>
+			<a href="/recipes/{result.id}">View Recipe</a>
+		{/each}
 	{/if}
-{/if}
+	{#if form && form.results.length === 0}
+		<p>no results</p>
+	{:else}
+		<div class="grid">
+			{#if form}
+				{#each form?.results[0] as recipe}
+					<div>
+						<div>
+							<h2>{recipe.title}</h2>
+							<p>{recipe.directions}</p>
+						</div>
+						<!-- TODO: this link is unreliable -->
+						<button onClick={goto('/recipes/' + recipe.id)}>View Recipe</button>
+					</div>
+				{/each}
+			{/if}
+		</div>
+	{/if}
+{/await}
